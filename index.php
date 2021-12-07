5e67c8426f86a1fb2884ceebad4f2180bef27a7f
Slava <gavrilin2410@gmail.com>
2021-12-07 20:24:07 +0300

    update manifest

--- index.php
+++ index.php
@@ -13,6 +13,14 @@
 		<meta name="description" content="Угадывайте по одному кадру фильмы, мультфильмы и сериалы" />
 		<meta name="keywords" content="Угадай фильм, кино, бесплатная браузерная игра, тест, играть без регистрации, онлайн, на время" />
 		<meta name="yandex-verification" content="22df35eea5fe9acf" />
+		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+		<link rel="manifest" href="/site.webmanifest">
+		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+		<meta name="msapplication-TileColor" content="#da532c">
+		<meta name="theme-color" content="#ffffff">
+
 		<!-- Yandex.Metrika counter -->
 		<script type="text/javascript" >
 		(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
