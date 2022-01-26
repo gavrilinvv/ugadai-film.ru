05ca7cda78b414195255e2a8a81d92ed1987d80d
Slava <gavrilin2410@gmail.com>
2022-01-26 12:01:32 +0300

    update ya metrika

--- index.php
+++ index.php
@@ -29,14 +29,14 @@
 		m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
 		(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-		ym(86702201, "init", {
+		ym(87285918, "init", {
 			clickmap:true,
 			trackLinks:true,
 			accurateTrackBounce:true,
 			webvisor:true
 		});
 		</script>
-		<noscript><div><img src="https://mc.yandex.ru/watch/86702201" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+		<noscript><div><img src="https://mc.yandex.ru/watch/87285918" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 		<!-- /Yandex.Metrika counter -->
 
 		<!-- Yandex.RTB -->
