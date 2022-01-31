a3d4b433cb7bc6bbbff2e963e94bf215deba10fc
Slava <gavrilin2410@gmail.com>
2022-01-31 21:42:07 +0300

    add type for favicon

--- index.php
+++ index.php
@@ -16,8 +16,8 @@
 		<meta name="keywords" content="Угадай фильм, кино, бесплатная браузерная игра, тест, играть без регистрации, онлайн, на время, сериалы, российские, советские, зарубежные, мультфильмы" />
 		<meta name="yandex-verification" content="22df35eea5fe9acf" />
 		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" type="image/x-icon">
+		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" type="image/x-icon">
 		<link rel="manifest" href="/site.webmanifest">
 		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
 		<meta name="msapplication-TileColor" content="#da532c">
