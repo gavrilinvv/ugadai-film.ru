a1b33b5ab6f4015a7bcddf400203abb58614c76d
Slava <gavrilin2410@gmail.com>
2023-11-23 21:16:43 +0300

    edit checkboxes size and add new films

--- index.php
+++ index.php
@@ -27,9 +27,7 @@
 		<!-- Yandex.Metrika counter -->
 		<? if (!$_GET["__cf_chl_tk"] && !$_GET["__cf_chl_rt_tk)"]) : ?>
 			<script type="text/javascript" >
-				console.log('1');
 				if (!window.location.search.includes('__cf_chl_tk') && !window.location.search.includes('__cf_chl_rt_tk')) {
-					console.log('2');
 					(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
 					m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
 					(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
